12522ee31edad76a682a6385364c0cb8a33b568b
ac1513 <ac1513@york.ac.uk>
2019-04-01 22:55:16 +0100

    fix plot titles

--- scripts/plot.py
+++ scripts/plot.py
@@ -15,6 +15,7 @@ args = parser.parse_args()
 in_file = args.in_file
 prefix = args.prefix
 
+
 with open(in_file, 'r') as text_file:
     files = text_file.read().strip().split()
 
@@ -26,13 +27,14 @@ for i in range(0, len(files), 30):
     counter += 1
     for file in sub_files:
         with open(file, 'r') as f:
+            file = str(file)
             df = pd.read_csv(f, index_col='contig')
             av_cov = str('{0:.1f}'.format(statistics.mean(df['cover'].tolist())))
             sd_cov = str('{0:.1f}'.format(statistics.stdev(df['cover'].tolist())))
             tot_len = str('{0:.1f}'.format(sum(df['length'].tolist())/1000))
             av_gc =  str('{0:.1f}'.format(statistics.mean(df['GC'].tolist())))
             sd_gc = str('{0:.1f}'.format(statistics.stdev(df['GC'].tolist())))
-            na = str(file.split('.')[0][8:])
+            na = str(file.split('/')[-1:][0].split('.')[0][8:])
             nu = str(len(df))
             axes1 = plt.subplot(gs[gsplace])
             x_range = [x for x in range(len(df.keys()[:-3]))]
@@ -48,9 +50,9 @@ for i in range(0, len(files), 30):
             plt.text(5, 0.00045, tot_len +'kb', fontsize=3)
             plt.text(5, 0.00002, 'GC% '+ av_gc +'±'+ sd_gc, fontsize=3)
             plt.tick_params(axis='both', labelsize=3, pad=-1, direction='out', length=2, width=0.5)
-            plt.tick_params(right='off', top='off')
+            plt.tick_params(right=False, top=False)
 #            plt.tight_layout()
             gsplace += 1
     plt.savefig('plots/' + str(counter) + '_' + prefix + '_plot.pdf', type='pdf')
-    print('Generated plot number ' + str(counter) + ' -> ' + str(counter) + '-' + prefix + '_plot.pdf')
+    print('Generated plot number ' + str(counter) + ' -> ' + str(counter) + '_' + prefix + '_plot.pdf')
     plt.close('all')
