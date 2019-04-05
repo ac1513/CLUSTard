2d39fbc28f1671fae87e2b6af9af1d1b4fddba32
ac1513 <ac1513@york.ac.uk>
2019-04-05 11:40:20 +0100

    done? add args too

--- scripts/start_feeder.py
+++ scripts/start_feeder.py
@@ -1,48 +1,35 @@
-# start_feeder.py
-# stand-alone version JC 13/06/17
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 13:50:48 2019
 
-import os
-import csv
-import numpy as np
+@author: ac1513
+"""
 
-path = os.path.dirname(os.path.realpath(__file__)) # path goes here
-dir = '/in_files/' # directory containing sequence files goes here
+import pandas as pd
+import argparse
 
-test_file = '/read_counts_derived.csv'
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('loc', help='location count files are in', type=str)
+parser.add_argument('jobid', help='location count files are in', type=str)
+args = parser.parse_args()
+loc = args.loc
+jobid = args.jobid
 
-print('Reading contig names')
-names = []
-with open(path+test_file, 'r') as f:
-	for line in f.readlines():
-		name, info = line.strip().split(',',1)
-		names.append((name))
+df = pd.read_csv(loc + '/' + jobid + '_read_counts_derived.csv', header=None, index_col = 0)
 
-# determine number of columns in file
-with open(path+test_file) as g:
-	col_num = g.readline().count(',')
+names = df.index.values.tolist()
 
-# set columns to be read
-col_range = []
-for loo in range(1, col_num, 1):
-	col_range.append(loo)
+col_num = len(df.columns)
 
-print('Reading contig values from columns '+str(col_range))
-values = np.genfromtxt(fname=path+test_file, delimiter=',', usecols=(col_range))
+df2 = df #seem to need to have a copy of the df to calc mean
 
-print('Calculating mean and standard deviation')
-b = np.c_[values, np.mean(values, axis = 1), np.std(values, ddof=1, axis=1)]
-diffs = np.array([col_range])
+df = df.drop(df.columns[len(df.columns)-1], axis=1) # drop last column so don't include it in stats - is still in df2..
 
-print('Calculating differences')
-for i in b:  # add '[:499]' for testing
-	a = np.array([[ ]])
-	for v in i[:col_num-1]:
-		a = np.append(a, v-i[col_num-1])
-	c = np.array([a])
-	diffs = np.append(diffs, c, axis=0)
+df2["mean"] = df.mean(axis=1)
+df2["sd"] = df.std(ddof = 1, axis=1)
 
-print('Concatenating results')
+diffs = df.sub(df.mean(axis=1), axis=0)
 
-np.save(path+'/CRISPR_names.out', names)
-np.save(path+'/CRISPR_b.out', b)
-np.save(path+'/CRISPR_diffs.out', diffs)
+diffs.to_csv(loc + '/' + jobid + '_diffs.csv') #diffs
+df2.to_csv(loc + '/' + jobid + '_values.csv') #diffs
