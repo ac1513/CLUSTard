cb8e6b18fe40046f659a8cb744d2bb4c87846fce
ac1513 <ac1513@york.ac.uk>
2019-04-29 14:46:39 +0100

    change to mean and range plot (+ works with py3)

--- scripts/plot.py
+++ scripts/plot.py
@@ -3,7 +3,7 @@
 
 import statistics
 import matplotlib
-matplotlib.use('pdf')
+#matplotlib.use('pdf')
 #%matplotlib inline
 import matplotlib.gridspec as gsp
 import matplotlib.pyplot as plt
@@ -31,6 +31,8 @@ if args.kraken_level:
 else:
     prefix = prefix
 
+matplotlib.rcParams['lines.linewidth'] = 0.5
+
 with open(in_file, 'r') as text_file:
     files = text_file.read().strip().split()
 
@@ -52,14 +54,24 @@ for i in range(0, len(files), 30):
             na = str(file.split('/')[-1:][0].split('.')[0][8:])
             nu = str(len(df))
             axes1 = plt.subplot(gs[gsplace])
+
+
             x_range = [x for x in range(len(df.keys()[:-3]))]
-            for index, row in df.iterrows():
-                y_range = row[:-3]
-                axes1.plot(x_range, y_range)
-            plt.semilogy(linewidth=0.25,mew=0.25)
+            y_range = df.mean()[:-3]
+            top = df.max()[:-3]
+            bottom = df.min()[:-3]
+
+
+            plt.plot(x_range, y_range, color='purple')
+            plt.fill_between(x_range, top, bottom, facecolor='grey', alpha=0.5)
+
+            plt.semilogy()
+
             x1,x2,y1,y2 = plt.axis()
-            plt.axis((x1,x2,0.00001,1))
-            plt.axhline(y=0.01, ls='--', lw = 0.5, c = 'black')
+            plt.axis((x1,x2,0.00001,10))
+
+            plt.axhline(y=0.01, ls='--', lw = 0.25, c = 'black')
+
             if kraken_file:
                 for line in open(kraken_file, 'r'):
                     if re.search(na, line):
@@ -69,14 +81,13 @@ for i in range(0, len(files), 30):
                         else:
                             per = line.split('\t')[1]
                             per = per.strip()
-                        plt.text(5, 0.1, per+'%:  ' + cont, fontsize=3)
-            plt.text(5, 0.4, na, fontsize = 3)
-            plt.text(5, 0.0001, nu+' cov:'+av_cov+'+/-'+sd_cov, fontsize=3)
-            plt.text(5, 0.00045, tot_len +'kb', fontsize=3)
-            plt.text(5, 0.00002, 'GC% '+ av_gc +'+/-'+ sd_gc, fontsize=3)
-            plt.tick_params(axis='both', labelsize=3, pad=-1, direction='out', length=2, width=0.5)
-            plt.tick_params(right=False, top=False)
+                        plt.text(0.5, 0.15, per+'%:  ' + cont, fontsize=2)
+            plt.text(0.5, 4, na, fontsize = 2, fontweight='bold')
+            plt.text(0.5, 1, nu+' cov:'+av_cov+'+/-'+sd_cov + ', ' + tot_len +'kb', fontsize=2)
+            plt.text(0.5, 0.4, 'GC% '+ av_gc +'+/-'+ sd_gc, fontsize=2)
+            plt.tick_params(axis='both', labelsize=2, pad=0, direction='out', length=1, width=0.25, right=False, top=False)
             gsplace += 1
-    plt.savefig('output/plots/' + str(counter) + '_' + prefix + '_plot.pdf', type='pdf')
+#    plt.show()
+    plt.savefig(str(counter) + '_' + prefix + '_plot.pdf', type='pdf', dpi=300)
     print('Generated plot number ' + str(counter) + ' -> ' + str(counter) + '_' + prefix + '_plot.pdf')
     plt.close('all')
