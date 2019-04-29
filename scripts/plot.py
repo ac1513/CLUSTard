81b8cf2fdb9a0f9eca55fe961fa4c308bf9fcc02
ac1513 <ac1513@york.ac.uk>
2019-04-29 14:55:49 +0100

    output location

--- scripts/plot.py
+++ scripts/plot.py
@@ -88,6 +88,6 @@ for i in range(0, len(files), 30):
             plt.tick_params(axis='both', labelsize=2, pad=0, direction='out', length=1, width=0.25, right=False, top=False)
             gsplace += 1
 #    plt.show()
-    plt.savefig(str(counter) + '_' + prefix + '_plot.pdf', type='pdf', dpi=300)
+    plt.savefig("output/plots/" + str(counter) + '_' + prefix + '_plot.pdf', type='pdf', dpi=300)
     print('Generated plot number ' + str(counter) + ' -> ' + str(counter) + '_' + prefix + '_plot.pdf')
     plt.close('all')
