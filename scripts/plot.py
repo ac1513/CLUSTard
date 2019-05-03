1564efca975625574be90f35959aa446facfdc0e
ac1513 <ac1513@york.ac.uk>
2019-05-03 13:06:12 +0100

    split up plot

--- scripts/plot.py
+++ scripts/plot.py
@@ -17,9 +17,11 @@ parser.add_argument('in_file', help='the name of the file containing a list of c
 parser.add_argument('prefix', help='prefix of the jobs', type=str)
 parser.add_argument('-k', '--kraken', help = 'merged kraken input file', type=str)
 parser.add_argument('-k_l', '--kraken_level', help = 'merged kraken input file', type=str)
+parser.add_argument('samples', help='samples.tsv file', type=str)
 args = parser.parse_args()
 in_file = args.in_file
 prefix = args.prefix
+samples = args.samples
 
 if args.kraken:
     kraken_file = args.kraken
@@ -31,13 +33,25 @@ if args.kraken_level:
 else:
     prefix = prefix
 
+set_groups = set()
+df_samples = pd.read_csv(samples, sep ='\t', index_col = 0)
+groups = df_samples["group"].tolist() #get rid of header
+
+for item in groups:
+    set_groups.add(item)
+dc = {}
+for item in list(set_groups):
+    dc[item] = -1
+for i in groups:
+    dc[i] +=1
+
 
 matplotlib.rcParams['lines.linewidth'] = 0.5
 matplotlib.rcParams['ytick.left'] = True
 matplotlib.rcParams['ytick.minor.size'] = 1
 matplotlib.rcParams['ytick.minor.width'] = 0.25
 matplotlib.rcParams['axes.linewidth'] = 0.5
-
+colours = ["crimson", "seagreen", "darkorange", "purple", "tab:pink", "darkslateblue", "tab:cyan"]
 
 with open(in_file, 'r') as text_file:
     files = text_file.read().strip().split()
@@ -62,15 +76,16 @@ for i in range(0, len(files), 30):
             axes1 = plt.subplot(gs[gsplace])
 
 
-            x_range = [x for x in range(len(df.keys()[:-3]))]
-            y_mean = df.mean()[:-3]
-            top = df.max()[:-3]
-            bottom = df.min()[:-3]
-
-
-            plt.plot(x_range, y_mean, color='purple')
-
-            plt.fill_between(x_range, top, bottom, facecolor='grey', alpha=0.5)
+            x_start = 0
+            for item in list(set_groups):
+                x_end = x_start + dc[item]
+                x_range = range(x_start, x_end)
+                y_mean = df.mean()[x_start:x_end]
+                top = df.max()[x_start:x_end]
+                bottom = df.min()[x_start:x_end]
+                plt.plot(x_range, y_mean, color=colours[item])
+                plt.fill_between(x_range, top, bottom, facecolor='gray', alpha=0.5)
+                x_start = x_start + dc[item]+1
 
             plt.semilogy()
 
