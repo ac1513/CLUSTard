6dc6d4eb4ec487f7bf1a83d366645595854ba2af
ac1513 <ac1513@york.ac.uk>
2019-06-14 16:36:18 +0100

    FIXED

--- scripts/parallel_merge_step2.py
+++ scripts/parallel_merge_step2.py
@@ -13,6 +13,12 @@ import csv
 import json
 import argparse
 
+
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
+
 short_list = []
 nr_list = []
 final_list = []
@@ -30,6 +36,27 @@ first_file = list_files[0]
 with open (first_file, 'r') as master:
 #	print('opening '+str(master))
 	master_list = json.load(master)
+	l = master_list
+	out = []
+	while len(l)>0:
+		first, *rest = l
+		first = set(first)
+		lf = -1
+		while len(first)>lf:
+			lf = len(first)
+			rest2 = []
+			for r in rest:
+				if len(first.intersection(set(r)))>0:
+					first |= set(r)
+				else:
+					rest2.append(r)
+				rest = rest2
+			out.append(first)
+		l = rest
+#	print(master_list)
+#	print("has changed to:")
+#	print(out)
+	master_list = out
 
 # open sequential files and merge into the master list if they match
 for current_f in list_files[1:]:
@@ -40,12 +67,36 @@ for current_f in list_files[1:]:
 			for row_2 in working_list:
 				if x & set(row_2):
 #					print('match between master and '+str(working_file))
+#					print(x, row_2)
 					x.update(row_2)
+#					print(x)
 					working_list.remove(row_2)
 	for entries in working_list: # add any sets left to the end of the master list
 #		print('adding unmatched entries from '+str(working_file))
 		y = [list(set(entries))]
 		master_list.extend(y)
 
+l = master_list
+out = []
+while len(l)>0:
+          first, *rest = l
+          first = set(first)
+          lf = -1
+          while len(first)>lf:
+                        lf = len(first)
+                        rest2 = []
+                        for r in rest:
+                                if len(first.intersection(set(r)))>0:
+                                        first |= set(r)
+                                else:
+                                        rest2.append(r)
+                                rest = rest2
+                        out.append(first)
+          l = rest
+#       print(master_list)
+#       print("has changed to:")
+#       print(out)
+master_list = out
+
 with open(write_file, 'w') as outgoing:
-	json.dump(master_list, outgoing)
+	json.dump(master_list, outgoing, default=set_default)
