f5577697c3de2ab03cfa62182dde117b5622d638
ac1513 <ac1513@york.ac.uk>
2021-12-09 20:42:19 +0000

    Update naming

--- workflow/scripts/python/singleton.py
+++ workflow/scripts/python/singleton.py
@@ -13,24 +13,24 @@ header = args.header
 counts_file = args.csv
 loc = args.loc
 
-input = loc
+singleton = loc
 
 header = ['contig'] + header + ['cover', 'length', 'GC']
 
-for singleton in input:
-    with open(singleton) as fasta:
-      name = fasta.readline()
-      name = name.strip(">\n")
-      faseq = fasta.readline()
-    with open(counts_file) as counts:
-      for line in counts:
-        if name in line:
-                count_line = line.strip('\n').split(',')
-                break
-    filename = str(singleton[:-6] + ".csv")
-    with open(filename, 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(header)
-        count_line.append(len(faseq))
-        count_line.append(su.GC(faseq))
-        writer.writerow(count_line)
+
+with open(singleton) as fasta:
+  name = fasta.readline()
+  name = name.strip(">\n")
+  faseq = fasta.readline()
+with open(counts_file) as counts:
+  for line in counts:
+    if name in line:
+            count_line = line.strip('\n').split(',')
+            break
+filename = str(singleton[:-6] + ".csv")
+with open(filename, 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(header)
+    count_line.append(len(faseq))
+    count_line.append(su.GC(faseq))
+    writer.writerow(count_line)
